f2300ca9496ded42b1c50f7c35644ed87e04991b
Zachary Viggiani <zach@bigmojo.net>
2017-05-11 13:19:06 -0400

    added plan and activities views anddcontrollers

---
+++ PlanIt/app/controllers/activities_controller.rb
@@ -0,0 +1,13 @@
+class ActivitiesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- PlanIt/config/routes.rb
+++ PlanIt/config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'plans/index'
+
+  get 'plans/show'
+
+  get 'plans/new'
+
+  get 'plans/edit'
+
+  get 'activities/index'
+
+  get 'activities/show'
+
+  get 'activities/new'
+
+  get 'activities/edit'
+
+  resources :users, only: [:new, :create]
 end
